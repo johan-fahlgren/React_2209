84d98142955b70c9d63effac05496c437bc22511
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-09-07 10:48:40 +0200

    remove

--- 2-lektion/my-react-app_GB/src/components/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Image = () => {
-	return (
-		<img src="/images/capy.jpg" alt="capy"/>
-	)
-}
-
-export default Image
--- 2-lektion/my-react-app_HM/src/components/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Image = () => {
-	return (
-		<div>
-			<img src="/images/capy.jpg" alt="alt text"/>
-		</div>
-	)
-}
